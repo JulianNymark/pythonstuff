474ae1388fda9e5e31c1b2b624adeb2c4c6e65c4
j <j@m.localhost>
2017-06-06 02:56:53 +0200

    :poop: but pygame is :snake:/:8ball:

--- spectrograph/spectrograph.py
+++ spectrograph/spectrograph.py
@@ -1,18 +1,8 @@
-# import matplotlib
-# matplotlib.use('Qt5Agg')
-# from matplotlib import pyplot as plt
-
-# from PIL import Image
-
 import pyaudio
 import sys
-import numpy as np
-import time
-from cv2 import *
+import pygame
 
-## matplotlib
-# fig = plt.gcf()
-# fig.canvas.set_window_title('matplotlib_float_')
+import numpy as np
 
 # No. of input samples used for a single FFT
 CHUNK_SIZE = 1024
@@ -29,16 +19,18 @@ HEIGHT = int(CHUNK_SIZE / 2)
 p = pyaudio.PyAudio()
 
 stream = p.open(format=pyaudio.paInt16,
-                    channels=1,
-                    rate=RATE,
-                    input=True,
-                    frames_per_buffer=CHUNK_SIZE,
-                    # input_device_index=3 # if default don't work, set this to
-                    # an audio device given by "python -m sounddevice"!
-                    )
+                channels=1,
+                rate=RATE,
+                input=True,
+                frames_per_buffer=CHUNK_SIZE,
+                input_device_index=12  # if default don't work, set this to
+                # an audio device given by "python -m sounddevice"!
+                )
 
 
 spectrogram = np.zeros([WINDOW_WIDTH, HEIGHT], dtype='uint16')
+
+i2 = 0
 
 while (True):
     data = stream.read(CHUNK_SIZE)
@@ -49,26 +41,23 @@ while (True):
 
     # Copy LAST WIDTH-1 columns from spectogram
     # to the FIRST WIDTH-1 columns in tmp
-    tmp[0:WINDOW_WIDTH-1, 0:HEIGHT] = spectrogram[1:WINDOW_WIDTH, 0:HEIGHT]
+    tmp[0:WINDOW_WIDTH - 1, 0:HEIGHT] = spectrogram[1:WINDOW_WIDTH, 0:HEIGHT]
 
     for i in range(1, HEIGHT):
         rvalue = abs(int(np.real(freq[i])))
-#        print(i2, i, rvalue)
+        print(i2, i, rvalue)
 
-        # circle(tmp, (WINDOW_WIDTH - 1, HEIGHT - i), 1, rvalue)
         tmp[-1, HEIGHT - i] = rvalue
 
     spectrogram = tmp
 
-    imshow('spectrogram', spectrogram)
-    time.sleep(5)
+    i2 += 1
 
-    ## PIL
-    # img = Image.fromarray(spectrogram)
-    # img.show()
-    ## matplotlib
-    # plt.imshow(spectrogram)
-    # plt.pause(0.05)
+    surface = pygame.surfarray.make_surface(spectrogram)
+
+    screen = pygame.display.set_mode((WINDOW_WIDTH, HEIGHT))
+    screen.blit(surface, (0, 0))
+    pygame.display.flip()
 
 stream.stop_stream()
 stream.close()
